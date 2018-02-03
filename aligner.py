61062b3a9b32a20983b6e83218c559118b9aa07c
Joseph Schiarizzi <jschiarizzi@gmail.com>
2018-02-03 14:06:55 -0800

    made it nicer to test

--- aligner.py
+++ aligner.py
@@ -8,18 +8,19 @@ import skimage as sk
 import skimage.io as skio
 
 # name of the input file
-imname = 'cathedral.jpg'
+imname = 'art.jpg'
 
 # read in the image
-im = skio.imread(imname)
+im = skio.imread('pics/' + imname)
 
 # convert to double (might want to do this later on to save memory)
 im = sk.img_as_float(im)
 
 # compute the height of each part (just 1/3 of total)
-height = np.floor(im.shape[0] / 3.0)
+height = int(np.floor(im.shape[0] / 3.0))
 
 # separate color channels
+
 b = im[:height]
 g = im[height: 2*height]
 r = im[2*height: 3*height]
@@ -31,10 +32,11 @@ r = im[2*height: 3*height]
 ### ag = align(g, b)
 ### ar = align(r, b)
 # create a color image
-im_out = np.dstack([ar, ag, b])
+im_out = np.dstack([r, g, b])
 
 # save the image
-fname = '/out_path/out_fname.jpg'
+fname = 'output/' + imname
+open(fname, 'w')
 skio.imsave(fname, im_out)
 
 # display the image
