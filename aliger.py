1632246918e00a5c640e155f314f431cc4d5c838
Joseph Schiarizzi <jschiarizzi@gmail.com>
2018-02-04 17:06:32 -0800

    outlined automation

--- aliger.py
+++ aliger.py
@@ -9,6 +9,59 @@ import skimage as sk
 import skimage.io as skio
 from matplotlib import pyplot as plt
 
+#######################################
+#                                     #
+# Helper functions                    #
+#                                     #
+#######################################
+#Move the image up 1 row
+def slideUp(image):
+    cols = image.shape[1] #is the number of columns.
+    temp = image
+
+    for a in range(cols):
+        image[:, a] = np.roll(temp[:, a], -1)
+    return image
+
+#move image 1 row down
+def slideDown(image):
+    cols = image.shape[1] #is the number of columns.
+    temp = image
+
+    for a in range(cols):
+        image[:, a] = np.roll(temp[:, a], 1)
+    return image
+
+#move image to the right 1
+def slideRight(image):
+
+
+#move image to the left
+def slideLeft(image):
+    
+
+
+#######################################
+#                                     #
+# aligning functions                  #
+#                                     #
+#######################################
+def align(image1, image2):
+    #count right slide:
+    #count up slide:
+    #track current
+
+    #slide image 1 up -8 -> 8
+        #slide image right
+
+
+
+
+#######################################
+#                                     #
+# Run the script and make a piture.   #
+#                                     #
+#######################################
 # name of the input file
 imname = 'lady.jpg'
 
